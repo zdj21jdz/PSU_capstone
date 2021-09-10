42bc1ddc2096775a3192d014ac1283db851d8e9d
zdj21jdz <zdj21jdz@gmail.com>
2021-09-10 10:10:37 -0700

    Adding current commit incase I need to rollback - rebuilding the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import './App.css';
+import main_nav from "./nav";
+
+const filler_p = (
+    <p>
+        Taking up some space to see what
+        it looks like
+    </p>
+);
 
 const App = () => {
     return (
-        <div>
-            <h1>Mal is awesome.</h1>
+        <>
+        { main_nav }
+
+        <div id="content">
+            <h1>PSU Trade Portal - Main Page</h1>
+            <h2>Hello, User!</h2>
+
+            { filler_p }
         </div>
+        
+        </>
     );
 }
 
