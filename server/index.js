d0a54994bd08a07f2265746e1598280c78a83411
zdj21jdz <zdj21jdz@gmail.com>
2021-09-11 10:57:42 -0700

    Adding database connection credentials

--- server/index.js
+++ server/index.js
@@ -2,3 +2,6 @@ const app = require('express')();
 const cors = require('cors');
 
 app.use(cors());
+
+const USER_LOGIN_CONNECTION_URL = process.env.USER_LOGIN_CONNECTION_URL
+const APP_CONNECTION_URL = process.env.APP_CONNECTION_URL
